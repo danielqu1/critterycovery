2219f6dfc09d38259c9ffd51d4f7aa6c5ff90d9f
wctech <wctech@cs.utexas.edu>
2021-03-05 23:21:41 -0600

    title add

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { randomBytes } from 'node:crypto';
 import React from 'react'; 
 import ReactPlayer from 'react-player';
 
@@ -25,7 +24,8 @@ const Home = () => {
               youtube: {
                 playerVars: { autoplay: 1, showinfo: 0, disablekb: 1, 
                               fs: 0, iv_load_policy: 3, loop: 1, 
-                              modestbranding: 0, rel: 0, pip: 0 }
+                              modestbranding: 0, rel: 0, pip: 0, 
+                              end: 1190}
               }
             }}
             muted={true}
@@ -33,6 +33,11 @@ const Home = () => {
             width='100vw'
             height='calc(100vw * .5625)'
           />
+
+          <div className='home-title'>
+            <p className='font-big'> Critters need Recovery</p>
+            <p className='font-medium-big'>If you love animals, you should support them</p>
+          </div>
           <h1>Welcome to critterycovery</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, veritatis. Soluta pariatur nesciunt voluptatum id incidunt minus ratione obcaecati laborum unde? Voluptatibus officia quia ducimus odio labore atque aperiam sint. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolores provident animi deserunt maxime. Officiis explicabo odit vitae. Doloremque nemo nobis voluptates ducimus aperiam libero maiores accusamus earum ipsam voluptate! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea nostrum commodi et exercitationem, maiores eum facere dicta repellendus laborum voluptatibus amet ipsam animi repellat distinctio deleniti quis? Dicta, aperiam nobis.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, veritatis. Soluta pariatur nesciunt voluptatum id incidunt minus ratione obcaecati laborum unde? Voluptatibus officia quia ducimus odio labore atque aperiam sint. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolores provident animi deserunt maxime. Officiis explicabo odit vitae. Doloremque nemo nobis voluptates ducimus aperiam libero maiores accusamus earum ipsam voluptate! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea nostrum commodi et exercitationem, maiores eum facere dicta repellendus laborum voluptatibus amet ipsam animi repellat distinctio deleniti quis? Dicta, aperiam nobis.</p>
