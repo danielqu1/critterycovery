9f88490b31d5bad62665f1126e965f91d937aecf
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2021-03-06 03:20:38 -0600

    all instance pages done

--- frontend/src/pages/instancepages/Germany.tsx
+++ frontend/src/pages/instancepages/Germany.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
+import img from '../countryPhotos/germany.jpeg';
 
 function Germany() {
     return(
-        <div>
-            Germany
+        <div className="text-center">
+            <h1>Germany</h1>
+            <img src={img}/>
+            <p>
+                Capital: Berlin <br/>
+                Location: Europe <br/>
+                Population: 81770900 <br/>
+                Area: 140,000 mi^2 <br/>
+                GDP: 3.9 trillion <br/>
+                Has habitat: <Nav.Link href='/habitats/Forest'>Forest</Nav.Link> <br/>
+                Includes endangered species: <Nav.Link href='/species/Jaguar'>Jaguar</Nav.Link>
+            </p>
         </div>
     )
 }
