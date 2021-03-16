536c2b11a62bed3eccd27d59650dc31a001f6a15
Brian Wang <bwang1008@gmail.com>
2021-03-16 01:13:46 -0500

    Got commits from all branches
    Closes #25

--- backend/gitlab.py
+++ backend/gitlab.py
@@ -2,25 +2,38 @@ import requests
 
 def stats():
 
-	commitsLink = "https://gitlab.com/api/v4/projects/24707879/repository/commits"
-	issuesLink = "https://gitlab.com/api/v4/projects/24707879/issues"
-	commitsResponse = requests.get(commitsLink).json()
-	issuesResponse = requests.get(issuesLink).json()
-	
+	branches = ['master', 'dev']
+
 	data = {"total": {"name": "total", "commits": 0, "issues": 0, "unittests": 0}}
 
-	for i in commitsResponse:
+	for branch in branches:
 
-		data["total"]["commits"] += 1
-		name = i["author_name"][:2].lower()
+		commitsLink = "https://gitlab.com/api/v4/projects/24707879/repository/commits?ref_name=" + branch + "&per_page=100"
+		#issuesLink = "https://gitlab.com/api/v4/projects/24707879/issues"
+		commitsResponse = requests.get(commitsLink).json()
+		#issuesResponse = requests.get(issuesLink).json()
+	
+		num = 0
 
-		if name[0] == 'w':			# handle William's two usernames
-			name = 'w';
+		for i in commitsResponse:
 
-		if name in data:
-			data[name]["commits"] += 1
-		else:
-			data[name] = {"name": name, "commits": 1, "issues": 0, "unittests": 0}
+			data["total"]["commits"] += 1
+			name = i["author_name"][:2].lower()
+
+			if name[0] == 'w':			# handle William's two usernames
+				name = 'w';
+
+			if name in data:
+				data[name]["commits"] += 1
+			else:
+				data[name] = {"name": name, "commits": 1, "issues": 0, "unittests": 0}
+
+			num += 1
+
+		#print("num =", num, " for branch ", branch)
+
+	issuesLink = "https://gitlab.com/api/v4/projects/24707879/issues"
+	issuesResponse = requests.get(issuesLink).json()
 
 	for i in issuesResponse:
 		if i["state"] == "closed":
