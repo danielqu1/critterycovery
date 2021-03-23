4ce01703455a2341dd8a76cea153e647773971d6
wctech <wctech@cs.utexas.edu>
2021-03-22 20:18:56 -0500

    some router preparations for changes to instance pages and addition of error page

---
+++ frontend/src/pages/Error.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Table, Nav } from 'react-bootstrap';
+
+function Error() {
+    return(
+        <div>
+            <h1>Error 404. Page not Found.</h1>
+        </div>
+    );
+}
+
+export default Error;
