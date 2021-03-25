7cf98e6e2eb739cd9d6374b811a6a3323c01facb
Brian Wang <bwang1008@gmail.com>
2021-03-25 02:10:15 -0500

    Save progress; cannot connect to database

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_cors import CORS
 from gitlab import stats					# gitlab.py
 # from dbstuff.create_tables import create_species_table
@@ -9,9 +9,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 
 app = Flask(
-    __name__,
-    static_folder="../frontend/build/static",
-    template_folder="../frontend/build"
+	__name__,
+	static_folder="../frontend/build/static",
+	template_folder="../frontend/build"
 )
 CORS(app)
 
@@ -23,81 +23,128 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_user}:{db_password}@{
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 manager = APIManager(app, flask_sqlalchemy_db=db)
+#manager.init_app(app)
 
 engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_name}", echo=False, future=True)
 # create_countries_table(engine)  # keep commented, do not run this again
 
 # model of Country for SQLAlchemy
 class Country(db.Model):
-    name = db.Column(db.Unicode, primary_key=True)
-    alpha2_code = db.Column(db.Unicode)
-    alpha3_code = db.Column(db.Unicode)
-    total_pop = db.Column(db.Integer)
-    capital = db.Column(db.Unicode)
-    region = db.Column(db.Unicode)
-    subregion = db.Column(db.Unicode)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    area = db.Column(db.Integer)
-    gini_index = db.Column(db.Float)
-    flag = db.Column(db.Unicode) # it's a link, is it a string?
+	name = db.Column(db.Unicode, primary_key=True)
+	alpha2_code = db.Column(db.Unicode)
+	alpha3_code = db.Column(db.Unicode)
+	total_pop = db.Column(db.Integer)
+	capital = db.Column(db.Unicode)
+	region = db.Column(db.Unicode)
+	subregion = db.Column(db.Unicode)
+	latitude = db.Column(db.Float)
+	longitude = db.Column(db.Float)
+	area = db.Column(db.Integer)
+	gini_index = db.Column(db.Float)
+	flag = db.Column(db.Unicode) # it's a link, is it a string?
 
 # model of Species for SQLAlchemy
 class Species(db.Model):
-    scientific_name = db.Column(db.Unicode, primary_key=True)
-    subspecies = db.Column(db.Unicode)
-    kingdom = db.Column(db.Unicode)
-    phylum = db.Column(db.Unicode)
-    _class = db.Column(db.Unicode)
-    _order = db.Column(db.Unicode)
-    family = db.Column(db.Unicode)
-    genus = db.Column(db.Unicode)
-    common_name = db.Column(db.Unicode)
-    population_trend = db.Column(db.Unicode)
-    marine = db.Column(db.Boolean)
-    freshwater = db.Column(db.Boolean)
-    terrestrial = db.Column(db.Boolean)
+	scientific_name = db.Column(db.Unicode, primary_key=True)
+	subspecies = db.Column(db.Unicode)
+	kingdom = db.Column(db.Unicode)
+	phylum = db.Column(db.Unicode)
+	_class = db.Column(db.Unicode)
+	_order = db.Column(db.Unicode)
+	family = db.Column(db.Unicode)
+	genus = db.Column(db.Unicode)
+	common_name = db.Column(db.Unicode)
+	population_trend = db.Column(db.Unicode)
+	marine = db.Column(db.Boolean)
+	freshwater = db.Column(db.Boolean)
+	terrestrial = db.Column(db.Boolean)
 
 # model of Habitat for SQLAlchemy
 class Habitat(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode)
-    marine = db.Column(db.Boolean) 
-    reported_marine_area = db.Column(db.Float)
-    reported_terrestrial_area = db.Column(db.Float)
-    countries = db.Column(db.Unicode)
-    iucn_category = db.Column(db.Integer)
-    designation = db.Column(db.Unicode)
-    link = db.Column(db.Unicode)
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.Unicode)
+	marine = db.Column(db.Boolean) 
+	reported_marine_area = db.Column(db.Float)
+	reported_terrestrial_area = db.Column(db.Float)
+	countries = db.Column(db.Unicode)
+	iucn_category = db.Column(db.Integer)
+	designation = db.Column(db.Unicode)
+	link = db.Column(db.Unicode)
 
 # model for connection between Species and Country
 class CountrySpeciesLink(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # this field needs to be added to db
-    scientific_name = db.Column(db.Unicode)
-    alpha2_code = db.Column(db.Unicode)
+	id = db.Column(db.Integer, primary_key=True)  # this field needs to be added to db
+	scientific_name = db.Column(db.Unicode)
+	alpha2_code = db.Column(db.Unicode)
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def index(path):
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route("/api/name")
 def name():
-    return {"name": "CRITTERYCOVERY"}
+	return {"name": "CRITTERYCOVERY"}
 
 @app.route("/api/gitlabstats")
 def gitlabstats():
-    return stats()
+	return stats()
+
+
+def create_endpoint(model, name, single=None):
+	if single is None:
+		postprocessors = {}
+	else:
+		postprocessors = {"GET_SINGLE" : [single]}
+
+	manager.create_api(model, methods=["GET"], collection_name=name, postprocessors=postprocessors)
+
+	
+
+
+def country_singleton(result):
+	links = Country.query.filter_by(name=result["name"]).all()
+	result["countries"] = [
+		{"id" : 5, "name" : "Beluga"}
+	]
 
 
 
-manager.create_api(Country, methods=["GET"], collection_name="countries")
-manager.create_api(Species, methods=["GET"], collection_name="species")
-manager.create_api(Habitat, methods=["GET"], collection_name="habitat")
+#manager.create_api(Country)
+#manager.create_api(Species, methods=["GET"], collection_name="species")
+#manager.create_api(Habitat, methods=["GET"], collection_name="habitat")
+
+create_endpoint(Country, "countries", country_singleton)
+
+
+
+@app.route("/api/countries")
+def get_country():
+
+	from sqlalchemy.orm import sessionmaker
+	Session = sessionmaker(bind=engine)
+
+	session = Session()
+
+	country_name = request.args.get('country')	# Spain
+
+	print("country name =", country_name)
+
+
+	my_country = session.query(Country).filter_by(name=country_name).first() 
+	
+	
+	session.close()
+	
+	
+	ret = {}
+	ret['blah'] = my_country
+	return ret
+
 
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, threaded=True, debug=True)
+	app.run(host="0.0.0.0", port=80, threaded=True, debug=True)
 
 
