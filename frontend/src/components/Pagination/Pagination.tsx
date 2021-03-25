991828cb5ab00af7934c1684d2fb4c45a2aa9093
Daniel Qu <d.qu@utexas.edu>
2021-03-25 17:48:30 -0500

    made pagination look kinda pretty

--- frontend/src/components/Pagination/Pagination.tsx
+++ frontend/src/components/Pagination/Pagination.tsx
@@ -1,28 +1,28 @@
 import React from 'react'; 
 import Pagination from 'react-bootstrap/Pagination'
 import PageItem from 'react-bootstrap/PageItem'
+import ReactPaginate from 'react-paginate';
 
+interface PaginationProps {
+    instancesPerPage: number,
+    totalInstances: number,
+    paginate: any,
+  }
 
 const Pagination_main = () => { 
     return ( 
         <>
-            <Pagination>
-                <Pagination.First />
-                <Pagination.Prev />
-                <Pagination.Item>{1}</Pagination.Item>
-                <Pagination.Ellipsis />
+             <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={50}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    containerClassName={"pagination"}
+                    activeClassName={"active"} />
 
-                <Pagination.Item>{10}</Pagination.Item>
-                <Pagination.Item>{11}</Pagination.Item>
-                <Pagination.Item active>{12}</Pagination.Item>
-                <Pagination.Item>{13}</Pagination.Item>
-                <Pagination.Item>{14}</Pagination.Item>
-
-                <Pagination.Ellipsis />
-                <Pagination.Item>{20}</Pagination.Item>
-                <Pagination.Next />
-                <Pagination.Last />
-            </Pagination>
         </>
     );
 };
