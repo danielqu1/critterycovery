02219aa49275ec6afb696059e890240da825de65
Brian Wang <bwang1008@gmail.com>
2021-03-27 22:22:48 -0500

    added basic structure of unittesting gui

--- selenium/guitest.py
+++ selenium/guitest.py
@@ -1,2 +1,12 @@
 import unittest
 
+class GuiTests(unittest.TestCase):
+	
+	def setUp(self):
+		# part of unittest library; called before every test 
+		pass
+
+	def tearDown(self):
+		# part of unittest library; called after every test
+		pass
+		
