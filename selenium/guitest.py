06fa97e6bcf5610ca95f4ba2253be0cc9ec7fb2e
Brian Wang <bwang1008@gmail.com>
2021-03-28 13:54:01 -0500

    Second test for actual website done yay

--- selenium/guitest.py
+++ selenium/guitest.py
@@ -12,8 +12,12 @@ from selenium.webdriver.chrome.options import Options		# configure options when 
 DRIVER_PATH = "./chromedriver_chrome89_win32.exe"
 # right now, don't use the linux one; does not seem to work'
 
+DEBUG = True
+
 URL = "https://critterycovery.me"
-#URL = "localhost:3000"						# debug ONLY; needs to have frontend (yarn start) open
+
+if DEBUG:
+	URL = "http://127.0.0.1:3000"						# debug ONLY; NEEDS http://, not https ; i don't think can have "localhost"
 
 class GuiTests(unittest.TestCase):
 	
@@ -22,8 +26,8 @@ class GuiTests(unittest.TestCase):
 		# arguments found here: https://peter.sh/experiments/chromium-command-line-switches/#no-sandbox
 
 		chrome_options = Options()
-		chrome_options.add_argument("--headless")		# no GUI / browser to show up when testing
-		chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])	# get rid of console message "DevTools listening on ws://127.0.0.1:53975/devtools/browser..."
+		#chrome_options.add_argument("--headless")		# no GUI / browser to show up when testing
+		#chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])	# get rid of console message "DevTools listening on ws://127.0.0.1:53975/devtools/browser..."
 		# chrome_options.add_argument("--no-sandbox")	# don't think we need this
 
 
@@ -37,26 +41,28 @@ class GuiTests(unittest.TestCase):
 		
 		self.assertIn("Python", driver.title)
 
-	def stest_main_page_0(self):
+	def test_main_page_0(self):
 		driver = self.driver
 		driver.get(URL)
 
-		print("title = " + driver.title)
+		print("title = " + driver.title)		# critterycovery
 
-		result = driver.find_elements_by_xpath("/html/body/div/div/body/div[2]/div/a[1]/div/div/div")[0].text	# get all instances of HTML with class card-body
-		
-		print("result =", result)
-		
-		self.assertEqual(len(result), "Species")							# we have 3 cards on main page
+		# just follow the HTML tags. If multiple, use [ ] brackes (1-indexed tho)
+		xpath = "/html/body/div/div/body/div[2]/div[3]/div/div[1]/a/div/div/div"
 
-		# driver.find_elements_by_xpath("/html/body/div/div/body/div[2]/div/a[1]/div/div/div")
+		if DEBUG:
+			xpath = "/html/body/div/div/body/div[2]/div/a[1]/div/div/div"
+
+		result = driver.find_elements_by_xpath(xpath)[0]	# traverse path in HTML, get first cuz "find_elements"
+		
+		self.assertEqual(result.text, "Species")							# we have 3 cards on main page
 
 	def test_about_page_0(self):
 		driver = self.driver
 
 		driver.get(URL + "/about")
 
-		print("title =" + driver.title) # critterycovery
+		# print("title =" + driver.title) # critterycovery
 
 		result = driver.find_elements_by_xpath("/html/body/div/div/div/div/h1")[0].text
 
