f319e9196bd044dc20b2f317c0fe883abf041d19
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2021-03-28 15:54:45 -0500

    minor change in countries model

--- backend/api.py
+++ backend/api.py
@@ -38,7 +38,7 @@ class countries_table(db.Model):
 	area = db.Column(db.Float)
 	gini_index = db.Column(db.Integer)
 	flag = db.Column(db.Unicode) # it's a link, is it a string?
-	embedded_map = db.Column(db.Unicode)
+	embedded_map_link = db.Column(db.Unicode)
 
 # model of Species for SQLAlchemy
 class species_table(db.Model):
@@ -100,7 +100,7 @@ class CountrySchema(ma.Schema):
 	area = fields.Float(required=False)
 	gini_index = fields.Integer(required=False)
 	flag = fields.String(required=False) # it's a link, is it a string?
-	embedded_map = fields.String(required=False)
+	embedded_map_link = fields.String(required=False)
 
 
 class SpeciesSchema(ma.Schema):
