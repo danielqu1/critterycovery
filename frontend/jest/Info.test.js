9dab606b4b19e66608b586a121bec6151818fecb
Brian Wang <bwang1008@gmail.com>
2021-03-29 16:44:53 -0500

    Fixed dependencies of jest

--- frontend/jest/Info.test.js
+++ frontend/jest/Info.test.js
@@ -2,4 +2,13 @@ import React from "react";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+import About from "../src/pages/about/About.tsx";
+
 configure({ adapter: new Adapter() });
+
+describe("Render views", () => {
+    test("About page", () => {
+        const aboutTest = shallow(<About />)
+        expect(aboutTest).toMatchSnapshot()
+    })
+})
