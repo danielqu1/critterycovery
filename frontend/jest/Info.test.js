a213ec93b38f3d1e53e8a7400f1fa6ac58fd9162
Brian Wang <bwang1008@gmail.com>
2021-03-29 18:01:40 -0500

    Added Home page to jest tests

--- frontend/jest/Info.test.js
+++ frontend/jest/Info.test.js
@@ -4,24 +4,33 @@ import Adapter from "enzyme-adapter-react-16";
 
 import "@testing-library/jest-dom/extend-expect"
 
+//import App from "../src/App.tsx"
 //import About from "../src/pages/about/About.tsx";     // Jest complains about interface in About.tsx
+
+import Home from "../src/pages/home/Home.tsx"
 import Error from "../src/pages/Error.tsx";
 import CountryModal from "../src/components/Modal/CountryModal.tsx";
 import HabitatModal from "../src/components/Modal/HabitatModal.tsx";
 import SpeciesModal from "../src/components/Modal/SpeciesModal.tsx";
 
-
-
 configure({ adapter: new Adapter() });
 
-describe("Render views", () => {
+describe("Pages", () => {
+    test("Home page", () => {
+        const homeTest = shallow(<Home />)
+        expect(homeTest).toMatchSnapshot()
+    });
+
     test("Error page", () => {
         const aboutTest = shallow(<Error />)
         expect(aboutTest).toMatchSnapshot()
     });
+});
 
+describe("Modals", () => {
+    
     test("Country Modal page", () => {
-        const aboutTest = shallow(<CountryModal/>)
+        const aboutTest = shallow(<CountryModal />)
         expect(aboutTest).toMatchSnapshot()
     });
 
@@ -34,4 +43,4 @@ describe("Render views", () => {
         const aboutTest = shallow(<SpeciesModal />)
         expect(aboutTest).toMatchSnapshot()
     });
-})
+});
