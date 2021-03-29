33e68464f43c7dce35db5371e7005ed77e92cdcf
Brian Wang <bwang1008@gmail.com>
2021-03-29 17:49:14 -0500

    Fixed interface keyword; still does not work on About

--- frontend/jest/Info.test.js
+++ frontend/jest/Info.test.js
@@ -5,7 +5,12 @@ import Adapter from "enzyme-adapter-react-16";
 import "@testing-library/jest-dom/extend-expect"
 
 //import About from "../src/pages/about/About.tsx";     // Jest complains about interface in About.tsx
-import About from "../src/pages/Error.tsx";
+import Error from "../src/pages/Error.tsx";
+import CountryModal from "../src/components/Modal/CountryModal.tsx";
+import HabitatModal from "../src/components/Modal/HabitatModal.tsx";
+import SpeciesModal from "../src/components/Modal/SpeciesModal.tsx";
+
+
 
 configure({ adapter: new Adapter() });
 
@@ -13,5 +18,20 @@ describe("Render views", () => {
     test("Error page", () => {
         const aboutTest = shallow(<Error />)
         expect(aboutTest).toMatchSnapshot()
-    })
+    });
+
+    test("Country Modal page", () => {
+        const aboutTest = shallow(<CountryModal/>)
+        expect(aboutTest).toMatchSnapshot()
+    });
+
+    test("Habitat Modal page", () => {
+        const aboutTest = shallow(<HabitatModal />)
+        expect(aboutTest).toMatchSnapshot()
+    });
+
+    test("Species Modal page", () => {
+        const aboutTest = shallow(<SpeciesModal />)
+        expect(aboutTest).toMatchSnapshot()
+    });
 })
