2f465cdfb351d9005855d810cb170e41adfa769d
wctech <wctech@cs.utexas.edu>
2021-04-07 17:43:32 -0500

    new frontpage! No scaling

---
+++ frontend/src/components/CardDecks/ModelDeck.tsx
@@ -0,0 +1,48 @@
+import { Col } from 'react-bootstrap';
+import ModelCard from '../Cards/ModelCard'
+
+import animal from '../../images/models/animal.png'
+import habitat from '../../images/models/habitat.png'
+import globe from '../../images/models/globe.png'
+  
+type Models = {
+    href: string,
+    title: string,
+    image: string,
+    text: string
+}
+
+
+function ModelDeck()  { 
+    let models: Models[] = [
+        { href: "/species",
+          title: "Species",
+          image: animal,
+          text: "Click here to go to species"
+        },
+        { href: "/habitats",
+          title: "Habitats",
+          image: habitat,
+          text: "Click here to go to habitats"
+        },
+        { href: "/countries",
+          title: "Countries",
+          image: globe,
+          text: "Click here to go to countries"
+        },
+    
+      ];
+    
+
+    const cards = []
+    for(let i = 0; i < models.length; i++){
+        cards.push(<Col style={{zIndex: 11}} ><ModelCard models={models[i]}></ModelCard></Col>)
+    }
+  return (
+    <>
+    {cards}
+    </>
+  ); 
+}; 
+  
+export default ModelDeck;
