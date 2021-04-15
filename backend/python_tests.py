606e6c64682f691f95d24cf42b0f25844f43c1ae
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2021-04-15 00:11:43 -0500

    added indiv country test

--- backend/python_tests.py
+++ backend/python_tests.py
@@ -27,6 +27,15 @@ class TestBackend(unittest.TestCase):
         r = requests.get(URL + "/api/species").json()
         self.assertEqual(len(r["species"]), 498)
 
+    def test_get_one_country(self):
+        r = requests.get(URL + "/api/countries/name=Afghanistan").json()
+        self.assertEqual(len(r["alpha2_code"]), "AF")
+        self.assertEqual(len(r["region"]), "Asia")
+
+    def test_get_one_habitat(self):
+
+    def test_get_one_species(self):
+
 
 if __name__ == "__main__":
     unittest.main()
