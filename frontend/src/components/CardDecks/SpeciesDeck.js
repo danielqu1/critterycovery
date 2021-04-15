c97211dad441dc87bc3be7dd2cb06a27f0467bf9
Brian Wang <bwang1008@gmail.com>
2021-04-15 03:43:00 -0500

    small reformatting

--- frontend/src/components/CardDecks/SpeciesDeck.js
+++ frontend/src/components/CardDecks/SpeciesDeck.js
@@ -8,62 +8,62 @@ import 'antd/dist/antd.css';
 import PaginationMain from '../../components/Pagination/Pagination';
 
 function SpeciesDeck(props)  { 
-    const offset = 3;
-    const [startingCard, setStart] = React.useState(0)
-    const [maxCardsShown, setCardsShown] = React.useState(10)
+	const offset = 3;
+	const [startingCard, setStart] = React.useState(0)
+	const [maxCardsShown, setCardsShown] = React.useState(10)
 
-    function giveData(){
+	function giveData(){
 		return props.species
 	};
-    const { Search } = Input;
+	const { Search } = Input;
 	const [searchVal, setSearchVal] = React.useState("");
 	const { filteredData, loading } = useTableSearch({
 		searchVal,
 		retrieve: giveData,
 	});
 
-    const speciesCards = [];
-    for (let i = startingCard; i < Math.min(startingCard + maxCardsShown, filteredData.length); i++) {
-        speciesCards.push(<Col className="container-fluid mt-4">
-            <a style={{ cursor: 'pointer' }} onClick={() => props.update(filteredData[i])}>
-            <SpeciesCard 
-                animal={filteredData[i]} 
-                photo={filteredData[i].image_link}
-                searchVal={searchVal}
-            ></SpeciesCard></a></Col>);
-    }
-    return(
-        <Container fluid className="justify-content-md-center">
-                <Row>
-                    <h1>{filteredData.length} Species. {maxCardsShown} per page</h1>
-                </Row>
-                <Row>
-                    <Search
-                        onChange={(e) => setSearchVal(e.target.value)}
-                        placeholder="Search"
-                        enterButton
-                        style={{
-                            position: "sticky",
-                            top: "0",
-                            left: "0",
-                            width: "200px",
-                            marginTop: "2vh"
-                        }}
-                    />
-                </Row>
-                <Row xs={1} sm={2} md={3} lg={4} xl={5}>
-                    {speciesCards}
-                </Row>
-                <PaginationMain 
-                    instancesPerPage= {maxCardsShown}
-                    totalInstances= {filteredData.length}
-                    startingInstance= {startingCard}
-                    offsetPagesShownFromCurrent= {offset}
-                    setStartingInstance= {setStart}
-                    setInstancesPerPage= {setCardsShown}
-                ></PaginationMain>
-            </Container>
-    )
+	const speciesCards = [];
+	for (let i = startingCard; i < Math.min(startingCard + maxCardsShown, filteredData.length); i++) {
+		speciesCards.push(<Col className="container-fluid mt-4">
+			<a style={{ cursor: 'pointer' }} onClick={() => props.update(filteredData[i])}>
+			<SpeciesCard 
+				animal={filteredData[i]} 
+				photo={filteredData[i].image_link}
+				searchVal={searchVal}
+			></SpeciesCard></a></Col>);
+	}
+	return(
+		<Container fluid className="justify-content-md-center">
+				<Row>
+					<h1>{filteredData.length} Species. {maxCardsShown} per page</h1>
+				</Row>
+				<Row>
+					<Search
+						onChange={(e) => setSearchVal(e.target.value)}
+						placeholder="Search"
+						enterButton
+						style={{
+							position: "sticky",
+							top: "0",
+							left: "0",
+							width: "200px",
+							marginTop: "2vh"
+						}}
+					/>
+				</Row>
+				<Row xs={1} sm={2} md={3} lg={4} xl={5}>
+					{speciesCards}
+				</Row>
+				<PaginationMain 
+					instancesPerPage= {maxCardsShown}
+					totalInstances= {filteredData.length}
+					startingInstance= {startingCard}
+					offsetPagesShownFromCurrent= {offset}
+					setStartingInstance= {setStart}
+					setInstancesPerPage= {setCardsShown}
+				></PaginationMain>
+			</Container>
+	)
 
 }
 
