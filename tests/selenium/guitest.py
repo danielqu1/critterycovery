ed209e089096c9cf8e06a6d8fcd0d558b7e076f6
Daniel Qu <d.qu@utexas.edu>
2021-04-16 17:21:14 -0500

    country tests

--- tests/selenium/guitest.py
+++ tests/selenium/guitest.py
@@ -171,8 +171,28 @@ class GuiTests(unittest.TestCase):
         xpath = "/html/body/div/div/div[2]/div/div[3]/div/div/div/div/div/div/table/tbody/tr/td[2]"
         result = self.driver.find_elements_by_xpath(xpath)[0] 
         self.assertEqual(result.text, "Greenfoot Quarry")
-
     
+    def test_countries_0(self):
+        self.driver.get(URL + "/countries")
+
+        xpath = "/html/body/div/div/div[2]/div/div/div/h1"
+        element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, xpath))
+        )
+        self.assertEqual(element.text, "Countries")
+    
+    def test_countries_0(self):
+        self.driver.get(URL + "/countries")
+
+        xpath_pag = "/html/body/div/div/div[2]/div/div[3]/div/div/div/ul/li[4]/a"
+        element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, xpath_pag))
+        )
+        self.driver.find_elements_by_xpath(xpath_pag)[0].click()
+
+        xpath = "/html/body/div/div/div[2]/div/div[3]/div/div/div/div/div/div/table/tbody/tr[3]/td[2]"
+        result = self.driver.find_elements_by_xpath(xpath)[0] 
+        self.assertEqual(result.text, "Belize")
 
     def tearDown(self):  # part of unittest library; called after every test
         # self.driver.close()  # from example
