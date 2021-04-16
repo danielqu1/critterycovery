aa936abc7944d6ba37b741ba4c1cabf3fb3e1908
Sahithi Golkonda <sahithigolkonda@Sreenivass-MacBook-Pro.local>
2021-04-15 23:32:58 -0500

    fixed empty fields in country pages

--- frontend/src/components/Modal/CountryModal.tsx
+++ frontend/src/components/Modal/CountryModal.tsx
@@ -10,6 +10,8 @@ interface habitats{
   name: string;
 }
 
+const no_info = "information not available"
+
 function CountryModal(props: any) {
   const [species, setSpecies] = React.useState(new Array<species>());
   const [habitats, setHabitats] = React.useState(new Array<habitats>());
@@ -39,13 +41,20 @@ function CountryModal(props: any) {
   for (let i = 0; i < species.length; i++) {
     speciesLinks.push(<a style={{ cursor: 'pointer' }} href={'/species/'+species[i].scientific_name}>{species[i].scientific_name+' '}</a>);
   }
+  if (species.length == 0) {
+		speciesLinks.push(<a>{no_info}</a>)
+	}
   speciesLinks.push(<br/>)
 
   const habitatLinks = [];
   for (let i = 0; i < habitats.length; i++) {
     habitatLinks.push(<a style={{ cursor: 'pointer' }} href={'/habitats/'+habitats[i].name}>{habitats[i].name+' '}</a>);
   }
+  if (habitats.length == 0) {
+		habitatLinks.push(<a>{no_info}</a>)
+	}
   habitatLinks.push(<br/>)
+  
   return (
     <Modal
       {...props}
@@ -62,14 +71,14 @@ function CountryModal(props: any) {
         <Image src={props.country.flag} rounded fluid style={{width:'50%'}}/><br/>
         alpha2 Code: {props.country.alpha2_code}<br/>
         alpha3 Code: {props.country.alpha3_code}<br/>
-        Total Population: {props.country.total_pop}<br/>
-        Capital: {props.country.capital}<br/>
-        Region: {props.country.region}<br/>
-        Subregion: {props.country.subregion}<br/>
-        Latitude: {props.country.latitude}&deg;<br/>
-        Longitude: {props.country.longitude}&deg;<br/>
-        Area: {props.country.area} km&sup2;<br/>
-        Gini Index: {props.country.gini_index}<br/>
+        Total Population: {props.country.total_pop ? props.country.total_pop : no_info}<br/>
+        Capital: {props.country.capital ? props.country.capital : "none"}<br/>
+        Region: {props.country.region ? props.country.region : "none"}<br/>
+        Subregion: {props.country.subregion ? props.country.subregion : "none"}<br/>
+        Latitude: {props.country.latitude ? props.country.latitude : no_info }&deg;<br/>
+        Longitude: {props.country.longitude ? props.country.longitude : no_info}&deg;<br/>
+        Area: {props.country.area ? props.country.area : no_info} km&sup2;<br/>
+        Gini Index: {props.country.gini_index ? props.country.gini_index : "none"}<br/>
         <iframe title="Map of Country"
           width="600"
           height="450"
