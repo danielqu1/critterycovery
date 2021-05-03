013706d4672394c31a8a4c7af6c42a3bebe461ce
Shaharyar Lakhani <shaharyar.lakhani@utexas.edu>
2021-05-03 18:40:33 -0500

    made first provider graph

--- frontend/src/pages/Visualizations.tsx
+++ frontend/src/pages/Visualizations.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import BarChart from '../viz/vizgraphs/BarChart'
+import providerIncidents from '../viz/vizdata/temp-atx-incidents.json'
 // vizData = ../components/vizdata/
 // code = ../components/vizgraphs/
 
 function Visualizations() {
     return (
         <div>
+            <br />
+            <h1>Provider Data</h1>
+            <hr />
+            <h3>Incidents Per Austin District</h3>
+            <BarChart
+                data={providerIncidents} xAttr="name" yAttr="value"
+                xLabel="Districts" yLabel="Incidents"
+            />
+            <hr />
         </div>
     )
 }
